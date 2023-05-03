a8745b7770e7e697b133d72dca9cf5fadd5c9e16
milan <milan@igrant.io>
2023-05-03 09:26:52 +0530

    Fix #15 - getting the code after login

--- lib/app/modules/Dexcom/controllers/dexcom_controller.dart
+++ lib/app/modules/Dexcom/controllers/dexcom_controller.dart
@@ -1,4 +1,3 @@
-
 import 'dart:ui';
 
 import 'package:webview_flutter/webview_flutter.dart';
@@ -7,13 +6,23 @@ import '/app/core/base/base_controller.dart';
 
 class DexcomController extends BaseController {
   late final WebViewController controller;
-  var clientID='Gf2kuGsl8xsvP2Ijdpywdwttto3Dzkrz';
+  var clientID = 'Gf2kuGsl8xsvP2Ijdpywdwttto3Dzkrz';
+  var redirectUri = "https://data4diabetes.com/abc";
+
+  //sandbox
+  //use this to get the code without login access(for testing)
+  var dexComBaseUrl = "https://sandbox-api.dexcom.com";
+
+  /*//prod
+  var dexComBaseUrl = "https://api.dexcom.com";*/
+
   @override
   void onInit() {
     DexcomLogin();
     super.onInit();
   }
-  DexcomLogin(){
+
+  DexcomLogin() {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
@@ -23,25 +32,30 @@ class DexcomController extends BaseController {
             return;
           },
           onPageStarted: (String url) {
+            //todo show progress on loading
             return;
           },
           onPageFinished: (String url) {
+            //todo stop progress after loading
             return;
           },
           onWebResourceError: (WebResourceError error) {
             return;
           },
           onNavigationRequest: (NavigationRequest request) {
-            // if (request.url.startsWith('https://www.youtube.com/')) {
-            // return NavigationDecision.prevent;
-            // }
+            if (request.url.startsWith(redirectUri)) {
+
+              //todo get the code from here then continue our process
+              print(request.url);
+
+              return NavigationDecision.prevent;
+            }
 
             return NavigationDecision.navigate;
           },
         ),
       )
       ..loadRequest(Uri.parse(
-          'https://api.dexcom.com/v2/oauth2/login?client_id=$clientID&response_type=code&scope=offline_access'));
+          '$dexComBaseUrl/v2/oauth2/login?client_id=$clientID&redirect_uri=$redirectUri&response_type=code&scope=offline_access'));
   }
-
 }
