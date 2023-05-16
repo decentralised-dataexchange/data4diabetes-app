4af38f9f07d8bf652d43a7262c7cde713c3c0a91
milan <milan@igrant.io>
2023-05-16 16:25:15 +0530

    Add #26 - crashlytics android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import '/app/my_app.dart';
@@ -5,7 +7,7 @@ import '/flavors/build_config.dart';
 import '/flavors/env_config.dart';
 import '/flavors/environment.dart';
 
-void main() {
+void main() async{
   EnvConfig prodConfig = EnvConfig(
     appName: "Flutter GetX Template Prod",
     baseUrl: "https://data4diabetes-staging-api.igrant.io/",
@@ -17,5 +19,8 @@ void main() {
     envConfig: prodConfig,
   );
 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   runApp(const MyApp());
 }
