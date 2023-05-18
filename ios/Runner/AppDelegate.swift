4f7f3460555353bab1bef3414e15695d5fad1bad
Rebin <rebin@igrant.io>
2023-05-18 20:41:38 +0530

    Data wallet SDK integrated to project

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
 import iGrantioSDK
-//import ama_ios_sdk
+import ama_ios_sdk
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,11 +11,16 @@ import iGrantioSDK
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+//    let flutterViewController = controller
+//      // Wrap the FlutterViewController inside a UINavigationController
+//      let navigationController = UINavigationController(rootViewController: flutterViewController)
+//      // Set the root view controller as the navigation controller
+//      window.rootViewController = navigationController
     let carePlanChannel = FlutterMethodChannel(name: "io.igrant.data4diabetes.channel",
                                                  binaryMessenger: controller.binaryMessenger)
-//      AriesMobileAgent.shared.configureWallet { success in
-//          debugPrint("Wallet configured --- \(success ?? false)")
-//      }
+      AriesMobileAgent.shared.configureWallet { success in
+          debugPrint("Wallet configured --- \(success ?? false)")
+      }
       carePlanChannel.setMethodCallHandler({
           (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
           switch call.method{
@@ -23,14 +28,14 @@ import iGrantioSDK
               iGrantioSDK.shared.modalPresentationStyle = .fullScreen
               iGrantioSDK.shared.show(organisationId: "638dd3b12f5d1700014431ec", apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2MzhkZTMzMDJmNWQxNzAwMDE0NDMxZjMiLCJvcmdpZCI6IiIsImVudiI6IiIsImV4cCI6MTcwMTM0NzQ2N30.2q7ENyEIXPRpQ1aF70jcF4XiQJs7YqOHwIogWXt1x5g", userId: "638de3302f5d1700014431f3")
               break
-//          case "Wallet":
-//              AriesMobileAgent.shared.showDataWalletHomeViewController()
-//          case "Connections":
-//              AriesMobileAgent.shared.showDataWalletConnectionsViewController()
-//          case "MySharedData":
-//              AriesMobileAgent.shared.showDataWalletShareDataHistoryViewController()
-//          case "Notifications":
-//              AriesMobileAgent.shared.showDataWalletNofificationViewController()
+          case "Wallet":
+              AriesMobileAgent.shared.showDataWalletHomeViewController(showBackButton: true)
+          case "Connections":
+              AriesMobileAgent.shared.showDataWalletConnectionsViewController()
+          case "MySharedData":
+              AriesMobileAgent.shared.showDataWalletShareDataHistoryViewController()
+          case "Notifications":
+              AriesMobileAgent.shared.showDataWalletNofificationViewController()
           default: break
           }
       })
