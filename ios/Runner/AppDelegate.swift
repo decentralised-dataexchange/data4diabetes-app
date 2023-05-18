2ebfdfb66e81f08938bd4aae6fcb57a7ee715516
Rebin <rebin@igrant.io>
2023-05-18 10:22:08 +0530

    firebase updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import iGrantioSDK
+//import ama_ios_sdk
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -12,6 +13,9 @@ import iGrantioSDK
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
     let carePlanChannel = FlutterMethodChannel(name: "io.igrant.data4diabetes.channel",
                                                  binaryMessenger: controller.binaryMessenger)
+//      AriesMobileAgent.shared.configureWallet { success in
+//          debugPrint("Wallet configured --- \(success ?? false)")
+//      }
       carePlanChannel.setMethodCallHandler({
           (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
           switch call.method{
@@ -19,6 +23,14 @@ import iGrantioSDK
               iGrantioSDK.shared.modalPresentationStyle = .fullScreen
               iGrantioSDK.shared.show(organisationId: "645a4172b9b055000150b248", apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NDVhNDE0YmI5YjA1NTAwMDE1MGIyNDciLCJvcmdpZCI6IiIsImVudiI6IiIsImV4cCI6MTcxNDc0MDg4Nn0.u6pBpv12ZfdHYMPoQHYR-oBR9ZOZVeHiChaQ8yiEMxE", userId: "5d8db1428e252f000180b5a6")
               break
+//          case "Wallet":
+//              AriesMobileAgent.shared.showDataWalletHomeViewController()
+//          case "Connections":
+//              AriesMobileAgent.shared.showDataWalletConnectionsViewController()
+//          case "MySharedData":
+//              AriesMobileAgent.shared.showDataWalletShareDataHistoryViewController()
+//          case "Notifications":
+//              AriesMobileAgent.shared.showDataWalletNofificationViewController()
           default: break
           }
       })
