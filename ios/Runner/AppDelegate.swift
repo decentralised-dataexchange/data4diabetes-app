317f19239e08c90f7c6589d782d2b69b0c51bb9f
Lijo George <lachulijo3@gmail.com>
2023-10-06 10:38:04 +0530

    #112 ios - privacy dashboard update
    ios - privacy dashboard update to latest

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -25,10 +25,8 @@ import SwiftMessages
             (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
             switch call.method{
             case "Preferences":
-                iGrantioSDK.shared.modalPresentationStyle = .fullScreen
-                iGrantioSDK.shared.changeSDKMode(mode: .demo)
-                iGrantioSDK.shared.show(organisationId: "645a4172b9b055000150b248", apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NDVhNDE0YmI5YjA1NTAwMDE1MGIyNDciLCJvcmdpZCI6IiIsImVudiI6IiIsImV4cCI6MTcxNDc0MDg4Nn0.u6pBpv12ZfdHYMPoQHYR-oBR9ZOZVeHiChaQ8yiEMxE", userId: "5d8db1428e252f000180b5a6")
-                break
+                BBConsentPrivacyDashboardiOS.shared.show(organisationId: "64f09f778e5f3800014a879a", apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NGYwYTYxZThlNWYzODAwMDE0YTg3YTYiLCJvcmdpZCI6IiIsImVudiI6IiIsImV4cCI6MTcyNDU5Njk2MX0.M3I6hJWtOyqbZXQwEGCK43AvROaoR_zncItmULpbFYE", userId: "64f0a61e8e5f3800014a87a6")
+               break
             case "Wallet":
                 AriesMobileAgent.shared.showDataWalletHomeViewController(showBackButton: true)
             case "Connections":
