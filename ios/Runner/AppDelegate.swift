1ef879d15e9fae4b0ae57ffd37ab8d967421117f
Lijo George <lachulijo3@gmail.com>
2023-10-13 15:54:02 +0530

    #118 ios - privacy dashboard turn On User Requests and turn On AskMe Section
     turnOnUserRequests
     turnOnAskMeSection

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -26,7 +26,9 @@ import SwiftMessages
             switch call.method{
             case "Preferences":
                 BBConsentPrivacyDashboardiOS.shared.show(organisationId: "64f09f778e5f3800014a879a", apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NGYwYTYxZThlNWYzODAwMDE0YTg3YTYiLCJvcmdpZCI6IiIsImVudiI6IiIsImV4cCI6MTcyNDU5Njk2MX0.M3I6hJWtOyqbZXQwEGCK43AvROaoR_zncItmULpbFYE", userId: "64f0a61e8e5f3800014a87a6")
-               break
+                BBConsentPrivacyDashboardiOS.shared.turnOnUserRequests = true
+                BBConsentPrivacyDashboardiOS.shared.turnOnAskMeSection = true
+                break
             case "Wallet":
                 AriesMobileAgent.shared.showDataWalletHomeViewController(showBackButton: true)
             case "Connections":
