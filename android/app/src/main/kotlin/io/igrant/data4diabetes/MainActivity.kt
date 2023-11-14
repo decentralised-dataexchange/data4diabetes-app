761fef64d4e2d73f1c7334cefecf7f629c48dbf2
Lijo George <lachulijo3@gmail.com>
2023-11-14 19:35:50 +0530

    change languageCode  as per app language
    change languageCode  as per app language

--- android/app/src/main/kotlin/io/igrant/data4diabetes/MainActivity.kt
+++ android/app/src/main/kotlin/io/igrant/data4diabetes/MainActivity.kt
@@ -63,15 +63,15 @@ class MainActivity : FlutterActivity() {
                 }
                 "Preferences" -> {
                     val apiKey: String? = call.argument("ApiKey")
-                    val orgId: String? = call.argument("orgId")
                     val baseUrl: String?= call.argument("baseUrl")
                     val userId: String?= call.argument("userId")
+                    val languageCode: String?= call.argument("languageCode")
                     PrivacyDashboard.showPrivacyDashboard().withApiKey(apiKey ?: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTY29wZXMiOlsic2VydmljZSJdLCJPcmdhbmlzYXRpb25JZCI6IjY0ZjA5Zjc3OGU1ZjM4MDAwMTRhODc5YSIsIk9yZ2FuaXNhdGlvbkFkbWluSWQiOiI2NTBhZTFmYmJlMWViNDAwMDE3MTFkODciLCJleHAiOjE3MzAyMjMyODh9.DlU8DjykYr3eBmbgsKLR4dnaChiRqXdxofKOuk4LiRM")
                         .withUserId(userId ?: "653fe90efec9f34efed23619")
                         .withBaseUrl(baseUrl ?: "https://demo-consent-bb-api.igrant.io/v2")
 //                        .enableUserRequest()
 //                        .enableAskMe()
-                        .withLocale("sv")
+                        .withLocale(languageCode ?: "sv")
                         .start(this)
                 }
                 "DataAgreementPolicy" -> {
