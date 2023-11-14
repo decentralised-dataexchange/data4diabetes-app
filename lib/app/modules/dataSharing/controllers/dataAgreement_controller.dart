c46bf812cc53e5399140ad045db103b11e34f615
Lijo George <lachulijo3@gmail.com>
2023-11-14 20:44:39 +0530

    remove unwanted commented codes

--- lib/app/modules/dataSharing/controllers/dataAgreement_controller.dart
+++ lib/app/modules/dataSharing/controllers/dataAgreement_controller.dart
@@ -20,104 +20,6 @@ class DataAgreementContoller extends BaseController {
   var thirdPartyOrgName = "Data4Diabetes";
   String? accessToken;
   var redirectUrl = "https://www.govstack.global/";
-  // invokeDataAgreement() {
-  //   controller = WebViewController()
-  //     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-  //     ..setBackgroundColor(const Color(0x00000000))
-  //     ..setNavigationDelegate(
-  //       NavigationDelegate(
-  //         onProgress: (int progress) {
-  //           // if (progress != maxProgressValue) {
-  //           //   showLoading();
-  //           // } else {
-  //           //   hideLoading();
-  //           // }
-  //
-  //           return;
-  //         },
-  //         onPageStarted: (String url) {
-  //           /// show progress on loading
-  //           // showLoading();
-  //
-  //           return;
-  //         },
-  //         onPageFinished: (String url) {
-  //           /// stop progress after loading
-  //           // hideLoading();
-  //
-  //           return;
-  //         },
-  //         onWebResourceError: (WebResourceError error) {
-  //           return;
-  //         },
-  //         onNavigationRequest: (NavigationRequest request) async {
-  //           if (request.url.startsWith('https://www.govstack.global/')) {
-  //             final uri = Uri.parse(request.url);
-  //             print('the response uri:${uri.queryParameters}');
-  //             if (uri.queryParameters.containsKey('error')) {
-  //               // Handle error response
-  //               String? error = uri.queryParameters['error'];
-  //               String? errorDescription = uri.queryParameters['errorDescription'];
-  //               print('Error: $error, Description: $errorDescription');
-  //               GetSnackToast(message: errorDescription??"something went wrong");
-  //               Get.off(LoginView());
-  //             } else if (uri.queryParameters.containsKey('credentials')) {
-  //               // Decode the base64-encoded credentials
-  //               print('got scueess data');
-  //               String? credentialsBase64 = uri.queryParameters['credentials'];
-  //               // Ensure that the base64 string has the correct length and padding
-  //               String paddedCredentialsBase64 = credentialsBase64! + '=' * (4 - credentialsBase64.length % 4);
-  //
-  //               // Decode the padded base64 string
-  //               List<int> bytes = base64.decode(paddedCredentialsBase64);
-  //               String decodedCredentialsJson = utf8.decode(bytes);
-  //
-  //               Map<String, dynamic> decodedCredentials = json.decode(decodedCredentialsJson);
-  //
-  //               // Accessing user information
-  //               Map<String, dynamic> userInfo = decodedCredentials['userInfo'];
-  //               String subject = userInfo['subject'];
-  //               String email = userInfo['email'];
-  //               bool emailVerified = userInfo['emailVerified'];
-  //
-  //               print('Subject: $subject');
-  //               print('Email: $email');
-  //               print('Email Verified: $emailVerified');
-  //
-  //               // Accessing token information
-  //               Map<String, dynamic> token = decodedCredentials['token'];
-  //               String accessToken = token['accessToken'];
-  //               int expiresIn = token['expiresIn'];
-  //               String refreshToken = token['refreshToken'];
-  //
-  //               print('Access Token: $accessToken');
-  //               print('Expires In: $expiresIn');
-  //               print('Refresh Token: $refreshToken');
-  //
-  //               SharedPreferences _prefs = await SharedPreferences.getInstance();
-  //               await _prefs.setString('dataSharingAccessToken',accessToken );
-  //               await _prefs.setInt('dataSharingTokenExpiresIn',expiresIn );
-  //               await _prefs.setString('dataSharingRefreshToken',refreshToken );
-  //               Get.back();
-  //               int index = _registerController.selectedPage.value + 1;
-  //               _registerController.pageController.animateToPage(index,
-  //                   duration: const Duration(milliseconds: 500),
-  //                   curve: Curves.ease);
-  //
-  //
-  //               GetSnackToast(message: 'Authorized successfully',color: Colors.green);
-  //             }
-  //
-  //             return NavigationDecision.navigate;
-  //           }
-  //
-  //           return NavigationDecision.navigate;
-  //         },
-  //       ),
-  //     )
-  //     ..loadRequest(Uri.parse(
-  //         'https://staging-consent-bb-iam.igrant.io/realms/3pp-application/protocol/openid-connect/auth?client_id=3pp&response_type=code&redirect_uri=https%3A%2F%2Fstaging-consent-bb-api.igrant.io%2Fv2%2Fservice%2Fdata-sharing%3FdataAgreementId%3D654cf0db9684ed907ce07c5f%26thirdPartyOrgName%3DData4Diabetes%26dataSharingUiRedirectUrl%3Dhttps%3A%2F%2Fwww.govstack.global%2F'));
-  // }
 
   invokeDataAgreement() {
     controller = WebViewController()
@@ -163,16 +65,8 @@ class DataAgreementContoller extends BaseController {
                 String decodedCredentialsJson = utf8.decode(bytes);
                 Map<String, dynamic> decodedCredentials =
                     json.decode(decodedCredentialsJson);
-                // Accessing user information
-                // Map<String, dynamic> userInfo = decodedCredentials['userInfo'];
-                // String subject = userInfo['subject'];
-                // String email = userInfo['email'];
-                // bool emailVerified = userInfo['emailVerified'];
-                // Accessing token information
                 Map<String, dynamic> token = decodedCredentials['token'];
                 accessToken = token['accessToken'];
-                // int expiresIn = token['expiresIn'];
-                // String refreshToken = token['refreshToken'];
                 SharedPreferences _prefs =
                     await SharedPreferences.getInstance();
                 await _prefs.setString('dataSharingAccessToken', accessToken!);
