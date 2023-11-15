1e5d7a1cb7cf6497683cc35b6d459f062ce03305
Lijo George <lachulijo3@gmail.com>
2023-11-15 13:26:57 +0530

    #180 ios configuration for Call the optInToDataAgreement function and getDataAgreement
     ios configuration for Call the optInToDataAgreement function and getDataAgreement

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -35,7 +35,7 @@ import SwiftMessages
                                            withUserId: userId ?? "6540952ffec9f34efed236c9",
                                            withOrgId: orgId ?? "64f09f778e5f3800014a879a",
                                            withBaseUrl: baseUrl ?? "https://demo-consent-bb-api.igrant.io/v2",
-                                           turnOnAske: false, turnOnUserRequest: false, turnOnAttributeDetail: false)
+                                           turnOnAskme: false, turnOnUserRequest: false, turnOnAttributeDetail: false)
                 break
             case "Wallet":
                 AriesMobileAgent.shared.showDataWalletHomeViewController(showBackButton: true)
@@ -74,6 +74,91 @@ import SwiftMessages
                 AriesMobileAgent.shared.changeSDKLanguage(languageCode: code)
             case "SharedData":
                 AriesMobileAgent.shared.showDataWalletScannerViewController()
+            case "DataSharing":
+                    let arguments = call.arguments as? [String: Any]
+                    let accessToken = arguments?["accessToken"] as? String
+                    let dataAgreementID = arguments?["dataAgreementID"] as? String
+                    let baseUrl = arguments?["baseUrl"] as? String
+                    var data: String? = nil
+
+                    PrivacyDashboard.configure(withApiKey: "", withUserId: "", withOrgId: "", withBaseUrl: String(baseUrl?.dropLast() ?? ""), accessToken: accessToken ?? "")
+                    PrivacyDashboard.updateDataAgreementStatus(dataAgreementId: dataAgreementID ?? "", status: true)
+                    PrivacyDashboard.receiveDataBackFromPrivacyDashboard = { dataReceived in
+                      let dict = dataReceived["consentRecord"] as? [String: Any]
+                      if let theJSONData = try? JSONSerialization.data(withJSONObject: dict ?? [:], options: .prettyPrinted),
+                        let theJSONText = String(data: theJSONData, encoding: String.Encoding.ascii) {
+                        print("JSON string = \n\(theJSONText)")
+                        data = theJSONText
+                      }
+
+                      if data != nil {
+                        result(data)
+                      } else {
+                        result(FlutterError(code: "DataSharing error", message: "Error occurred. Data: \(data ?? "")", details: nil))
+                      }
+                    }
+            case "GetDataAgreement":
+                    let arguments = call.arguments as? [String: Any]
+                    let apiKey = arguments?["apiKey"] as? String
+                    let userId = arguments?["userId"] as? String
+                    let dataAgreementID = arguments?["dataAgreementID"] as? String
+                    let baseUrl = arguments?["baseUrl"] as? String
+                    var data: String? = nil
+
+                    PrivacyDashboard.configure(withApiKey: apiKey ?? "", withUserId: userId ?? "", withOrgId: "", withBaseUrl: String(baseUrl?.dropLast() ?? ""), accessToken: "")
+                    PrivacyDashboard.readDataAgreementApi(dataAgreementId: dataAgreementID ?? "") { success, resultVal in
+                      debugPrint("Data receieved here:\(resultVal)")
+                      let dict = resultVal["dataAgreement"] as? [String: Any]
+                      if let theJSONData = try? JSONSerialization.data(withJSONObject: dict ?? [:], options: .prettyPrinted),
+                        let theJSONText = String(data: theJSONData, encoding: String.Encoding.ascii) {
+                        debugPrint("JSON string = \n\(theJSONText)")
+                        data = theJSONText
+                      }
+
+                      if data != nil {
+                        result(data)
+                      } else {
+                        result(FlutterError(code: "DataSharing error", message: "Error occurred. Data: \(data ?? "")", details: nil))
+                      }
+                    }
+            case "GetDataAgreementWithApiKey":
+                    let arguments = call.arguments as? [String: Any]
+                    let apiKey = arguments?["apiKey"] as? String
+                    let userId = arguments?["userId"] as? String
+                    let dataAgreementID = arguments?["dataAgreementID"] as? String
+                    let baseUrl = arguments?["baseUrl"] as? String
+                    var data: String? = nil
+
+                    PrivacyDashboard.configure(withApiKey: apiKey ?? "", withUserId: userId ?? "", withOrgId: "", withBaseUrl: String(baseUrl?.dropLast() ?? ""), accessToken: "")
+                    PrivacyDashboard.readDataAgreementApi(dataAgreementId: dataAgreementID ?? "") { success, resultVal in
+                      debugPrint("Data receieved here:\(resultVal)")
+                      let dict = resultVal["dataAgreement"] as? [String: Any]
+                      if let theJSONData = try? JSONSerialization.data(withJSONObject: dict ?? [:], options: .prettyPrinted),
+                        let theJSONText = String(data: theJSONData, encoding: String.Encoding.ascii) {
+                        debugPrint("JSON string = \n\(theJSONText)")
+                        data = theJSONText
+                      }
+
+                      if data != nil {
+                        result(data)
+                      } else {
+                        result(FlutterError(code: "DataSharing error", message: "Error occurred. Data: \(data ?? "")", details: nil))
+                      }
+                    }
+            case "CreateIndividual":
+                    var data: String? = nil
+                    let dict = ["id":"6553af58ec660dae93f6e254", "externalId": "","externalIdType":"", "identityProviderId":"", "name": "Flutter app", "iamId": "a2bd807c-fde4-40e8-8bfd-ff715d596492", "email":"flutterapp@yupmail.com", "phone": "9876987698"]
+                    let individualDict = ["individual": dict]
+                    if let jsonData = try? JSONSerialization.data(withJSONObject: individualDict, options: .prettyPrinted),
+                      let theJSONText = String(data: jsonData, encoding: String.Encoding.ascii) {
+                       debugPrint("JSON string = \n\(theJSONText)")
+                       data = theJSONText
+                    }
+                    if data != nil {
+                      result(data)
+                    } else {
+                      result(FlutterError(code: "DataSharing error", message: "Error occurred. Data: \(data ?? "")", details: nil))
+                    }
             default: break
             }
         })
