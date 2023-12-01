476b5c9b515b1296960f199f6e5acac1daf882e5
Lijo George <lachulijo3@gmail.com>
2023-12-01 18:09:20 +0530

    #235 draw a thin line around the attribute box
    To draw a thin line around the attribute box in the registration screen

--- lib/app/core/values/app_colors.dart
+++ lib/app/core/values/app_colors.dart
@@ -14,6 +14,7 @@ abstract class AppColors {
   static const Color colorLightGreen = Color(0xFF00EFA7);
   static const Color colorWhite = Color(0xFFFFFFFF);
   static const Color lightGreyColor = Color(0xFFC4C4C4);
+  static const Color veryLightGreyColor=Color(0xFFEEEEEE);
   static const Color errorColor = Color(0xFFAB0B0B);
   static const Color colorDark = Color(0xFF323232);
 
