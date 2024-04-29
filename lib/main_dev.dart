7363657a208f7aac9779782ca85e2f84ddacd5f2
Lijo George <lachulijo3@gmail.com>
2024-04-29 10:28:06 +0530

    Fix: #309 - Resolved app name discrepancy in android

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -9,7 +9,7 @@ import '/flavors/environment.dart';
 
 void main() async {
   EnvConfig devConfig = EnvConfig(
-    appName: "Flutter GetX Template Dev",
+    appName: "Data4Diabetes",
     baseUrl: "https://data4diabetes-staging-api.igrant.io/",
     dexComBaseUrl: "https://sandbox-api.dexcom.com",
     shouldCollectCrashLog: true,
