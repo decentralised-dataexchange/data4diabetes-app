00ebaedb9bd54f5fa112a704f041ae1ec377c15a
Lijo George <lachulijo3@gmail.com>
2025-10-08 10:55:03 +0530

    Fix: #82 - Implemented popup UI view of data wallet

--- lib/app/core/widget/app_bar_with_logo.dart
+++ lib/app/core/widget/app_bar_with_logo.dart
@@ -1,10 +1,14 @@
+import 'package:Data4Diabetes/app/modules/home/controllers/home_controller.dart';
 import 'package:Data4Diabetes/app/modules/insights/controllers/insights_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../Constants/Palette.dart';
 import '../../modules/settings/views/settings_view.dart';
+import 'dart:math';
+
 class AppBarWithLogo extends StatelessWidget  implements PreferredSizeWidget {
   var title;
   bool? isInsight;
@@ -13,6 +17,8 @@ class AppBarWithLogo extends StatelessWidget  implements PreferredSizeWidget {
   static const double circleContainerWidth = 50;
   static const double circleContainerHeight = 50;
   final InsightsController _insightsController = Get.find();
+  final HomeController _homeController = Get.find();
+
   @override
   Size get preferredSize => AppBar().preferredSize;
   @override
@@ -45,12 +51,9 @@ class AppBarWithLogo extends StatelessWidget  implements PreferredSizeWidget {
       leading: Container(
         height: circleContainerHeight,
         width: circleContainerWidth,
-
         decoration: const BoxDecoration(
           shape: BoxShape.circle,
         ),
-
-        //backgroundImage: AssetImage('images/blood_drop.png'),
         child: Image.asset(
           'images/blood_drop.png',
           fit: BoxFit.cover,
@@ -60,34 +63,79 @@ class AppBarWithLogo extends StatelessWidget  implements PreferredSizeWidget {
         'Data4Diabetes',
         style: TextStyle(
             color: Color.fromARGB(255, 30, 189, 174),
-            fontSize: 25,
+            fontSize: 20,
             fontWeight: FontWeight.bold),
       ),
       subtitle: Text(title),
-      trailing: IconButton(
-        icon: const Icon(
-          Icons.settings_outlined,
-          color: Colors.blueGrey,
-          size: 30,
-        ),
-        onPressed: () {
-          Get.to(SettingsView())?.then((value) async {
-            if(isInsight==true){
-              SharedPreferences _prefs = await SharedPreferences.getInstance();
-              var token = _prefs.getString('access_token');
-              if (token != null) {
-                _insightsController.estimatedGlucoseValues();
-                _insightsController.gMICalculator(
-                    _insightsController.selectedValue.value = 'TODAY');
-                _insightsController.tIRCalculator(
-                    _insightsController.selectedValue.value = 'TODAY');
-                _insightsController.addChartDataValues(
-                    _insightsController.selectedValue.value = 'TODAY');
-              }
-            }
-          });
-        },
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min, // Important: keeps the Row compact
+        children: [
+          IconButton(
+            icon: const Icon(
+              Icons.refresh_outlined, // <-- your new icon
+              color: Colors.blueGrey,
+              size: 25,
+            ),
+            onPressed: () {
+              // Generate random glucose value
+              final double glucoseValue = getRandomGlucoseValue();
+
+              print("Generated Glucose Value Lijo: $glucoseValue");
+
+              _homeController.updateGlucose(glucoseValue);
+
+              // Build a Map<String, String> for attributes
+              final Map<String, String> attributesMap = {
+                "glucoseValue": glucoseValue.toString(),
+              };
+
+              // MethodChannel to call Kotlin
+              const platform = MethodChannel('io.igrant.data4diabetes.channel');
+
+              // Invoke the Kotlin method
+              platform.invokeMethod('addSelfAttestedCredential', {
+                "title": "Self Attested",
+                "description": "Self Attested Glucose Value",
+                "attributesMap": attributesMap,
+                "connectionName": "Data4Diabetes",
+                "location": "sweden",
+                "vct": "open_id_for_self_attested_credentials",
+                "issuedDate": DateTime.now().millisecondsSinceEpoch,
+              });
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.settings_outlined,
+              color: Colors.blueGrey,
+              size: 25,
+            ),
+            onPressed: () {
+              Get.to(SettingsView())?.then((value) async {
+                if (isInsight == true) {
+                  SharedPreferences _prefs = await SharedPreferences.getInstance();
+                  var token = _prefs.getString('access_token');
+                  if (token != null) {
+                    _insightsController.estimatedGlucoseValues();
+                    _insightsController.gMICalculator(
+                        _insightsController.selectedValue.value = 'TODAY');
+                    _insightsController.tIRCalculator(
+                        _insightsController.selectedValue.value = 'TODAY');
+                    _insightsController.addChartDataValues(
+                        _insightsController.selectedValue.value = 'TODAY');
+                  }
+                }
+              });
+            },
+          ),
+        ],
       ),
+
     );
   }
+}
+double getRandomGlucoseValue() {
+  final random = Random();
+  double value = 4.0 + random.nextDouble() * (7.0 - 4.0);
+  return double.parse(value.toStringAsFixed(1));
 }
--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,6 +2,19 @@ import 'package:flutter/services.dart';
 
 import '/app/core/base/base_controller.dart';
 
+import 'package:flutter/services.dart';
+import '/app/core/base/base_controller.dart';
+import 'package:get/get.dart';
+
 class HomeController extends BaseController {
   var platform = const MethodChannel('io.igrant.data4diabetes.channel');
+
+  // Reactive glucose value
+  var glucoseValue = 0.0.obs; // initial default value
+
+  // Method to update glucose
+  void updateGlucose(double value) {
+    glucoseValue.value = value;
+  }
 }
+
